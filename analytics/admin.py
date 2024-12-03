43d90df01171c206f92d3983fe5d3dfc90a886e7
Alona <alona.sorochynska@gmail.com>
2024-12-03 11:35:36 +0000

    Revert "add admin.py for analytics"
    This reverts commit 88757f0db69db71b2c2d2b9da44455409ebc63f4.

--- analytics/admin.py
+++ analytics/admin.py
@@ -1,10 +1,3 @@
 from django.contrib import admin
-from analytics.models import PageVisit
 
-
-@admin.register(PageVisit)
-class PageVisitAdmin(admin.ModelAdmin):
-    list_display = ("session_key", "path", "ip_address", "country", "city", "created_at")
-    search_fields = ("session_key", "path", "ip_address", "country", "city")
-    list_filter = ("country", "city", "created_at")
-    date_hierarchy = "created_at"
+# Register your models here.
