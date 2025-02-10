57f0a1229c5cf257e7177b25742a5c5740e876da
Alona <alona.sorochynska@gmail.com>
2025-02-10 11:38:30 +0000

    collect HTTP_X_FORWARDED_FOR

--- analytics/middleware.py
+++ analytics/middleware.py
@@ -30,9 +30,9 @@ class AnalyticsMiddleware:
 
     @staticmethod
     def get_client_ip(request):
-        x_forwarded_for = request.META.get("HTTP_X_REAL_IP")
+        x_forwarded_for = request.META.get("HTTP_X_FORWARDED_FOR")
         if x_forwarded_for:
-            ip = x_forwarded_for.split(",")[0]
+            ip = x_forwarded_for.split(",")[0].strip()
         else:
             ip = request.META.get("REMOTE_ADDR")
         return ip
