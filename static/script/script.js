a8af67682f657e2e0dec2b51febdab1ec143d39b
Alona <alona.sorochynska@gmail.com>
2025-02-11 09:28:02 +0000

    close navbar by touching empty space

--- static/script/script.js
+++ static/script/script.js
@@ -5,19 +5,31 @@ document.addEventListener("DOMContentLoaded", function() {
   const closeButton = menuElement.querySelector(".menu-close-btn");
   const navbar = document.querySelector(".navbar");
 
-  menuIcon.addEventListener("click", function() {
+  menuIcon.addEventListener("click", function(event) {
     menuElement.classList.add("menu-opened");
     navbar.classList.add("open");
     menuIcon.style.display = "none";
     closeButton.style.display = "inline-block";
+    event.stopPropagation();
   });
 
-  closeButton.addEventListener("click", function() {
+  closeButton.addEventListener("click", function(event) {
+    closeMenu();
+    event.stopPropagation();
+  });
+
+  document.addEventListener("click", function(event) {
+    if (!menuElement.contains(event.target)) {
+      closeMenu();
+    }
+  });
+
+  function closeMenu() {
     menuElement.classList.remove("menu-opened");
     navbar.classList.remove("open");
     closeButton.style.display = "none";
     menuIcon.style.display = "inline-block";
-  });
+  }
 
   const navbarItems = document.querySelectorAll(".navbar-item");
   const currentUrl = window.location.href;
