71babf0eee88eaf7ac20ca5f6e14c2fc3efa0279
Alona <alona.sorochynska@gmail.com>
2025-02-12 10:31:41 +0000

    close mobile navbar touching empty screen

--- static/script/script.js
+++ static/script/script.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
   const menuElement = document.getElementById("js-menu");
   const menuIcon = menuElement.querySelector(".burger-menu");
-  const menuText = menuElement.querySelector(".menu-text");
   const closeButton = menuElement.querySelector(".menu-close-btn");
   const navbar = document.querySelector(".navbar");
+  const navbarItems = document.querySelectorAll(".navbar-item");
 
   menuIcon.addEventListener("click", function(event) {
     menuElement.classList.add("menu-opened");
@@ -18,8 +18,14 @@ document.addEventListener("DOMContentLoaded", function() {
     event.stopPropagation();
   });
 
-  document.addEventListener("click", function(event) {
-    if (!menuElement.contains(event.target)) {
+  navbarItems.forEach(item => {
+    item.addEventListener("click", function(event) {
+      event.stopPropagation();
+    });
+  });
+
+  window.addEventListener("click", function(event) {
+    if (!menuElement.contains(event.target) && !menuIcon.contains(event.target)) {
       closeMenu();
     }
   });
@@ -30,12 +36,4 @@ document.addEventListener("DOMContentLoaded", function() {
     closeButton.style.display = "none";
     menuIcon.style.display = "inline-block";
   }
-
-  const navbarItems = document.querySelectorAll(".navbar-item");
-  const currentUrl = window.location.href;
-  navbarItems.forEach(item => {
-    if (item.href === currentUrl) {
-      menuText.textContent = item.textContent;
-    }
-  });
 });
